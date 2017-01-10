85aaea76cd44c685d582bf0a174b36b0f42eee0d
eparkhurst <eli.parkhurst@gmail.com>
2017-01-10 13:22:05 -0700

    added code road trip,gingerMingel and painted with fire screen shots and finally figured out how to hide the cover on hover...opacity:0

--- script.js
+++ script.js
@@ -16,28 +16,4 @@ $(document).ready(function(){
   $(".skibox").click(function(){
       $(".skiDescription").toggle();
   })
-
-  $(".pi1").hover(function(){
-    $(".c1").toggle();
-    $('.gitup').css({
-      '-webkit-transform' : 'scale(' + 1.1 + ')',
-      '-moz-transform'    : 'scale(' + 1.1 + ')',
-      '-ms-transform'     : 'scale(' + 1.1 + ')',
-      '-o-transform'      : 'scale(' + 1.1 + ')',
-      'transform'         : 'scale(' + 1.1 + ')'
-    });
-  })
-  $(".pi2").hover(function(){
-    $(".c2").toggle();
-  })
-  $(".pi3").hover(function(){
-    $(".c3").toggle();
-  })
-  $(".pi4").hover(function(){
-    $(".c4").toggle();
-  })
-  $(".pi5").hover(function(){
-    $(".c5").toggle();
-  })
-
 });
